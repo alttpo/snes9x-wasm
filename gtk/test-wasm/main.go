7b105a06af9d386db681ca6ef37470c1b6081cf3
jsd1982 <james.jdunne@gmail.com>
2023-05-12 17:04:08 -0500

    wasm: example Go code to read from VRAM

--- gtk/test-wasm/main.go
+++ gtk/test-wasm/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"io"
-	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -14,40 +15,47 @@ type ReaderWriterAt interface {
 }
 
 var (
+	romFile  ReaderWriterAt
 	wramFile ReaderWriterAt
+	vramFile ReaderWriterAt
 	nmiFile  io.Reader
 )
-
-func dummy() int {
-	return rand.Int()
-}
-
-func main_bench() {
-	for j := 0; j < 20; j++ {
-		start := time.Now()
-		x := 0
-		for i := 0; i < 1_000_000; i++ {
-			x += dummy()
-		}
-		end := time.Now()
-		fmt.Printf("%v\n", x)
-		fmt.Printf("delta %v\n", end.Sub(start).Nanoseconds())
-	}
-}
 
 func main() {
 	var err error
 
-	var f *os.File
+	var fWRAM *os.File
 	fmt.Println("opening wram")
-	f, err = os.OpenFile("/tmp/snes/mem/wram", os.O_RDWR, 0666)
+	fWRAM, err = os.OpenFile("/tmp/snes/mem/wram", os.O_RDWR, 0666)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "open(wram): %v\n", err)
 		return
 	}
-	defer f.Close()
-	fmt.Printf("fd: %d\n", f.Fd())
-	wramFile = f
+	defer fWRAM.Close()
+	fmt.Printf("fd: %d\n", fWRAM.Fd())
+	wramFile = fWRAM
+
+	var fVRAM *os.File
+	fmt.Println("opening vram")
+	fVRAM, err = os.OpenFile("/tmp/snes/mem/vram", os.O_RDWR, 0666)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "open(vram): %v\n", err)
+		return
+	}
+	defer fVRAM.Close()
+	fmt.Printf("fd: %d\n", fVRAM.Fd())
+	vramFile = fVRAM
+
+	var fROM *os.File
+	fmt.Println("opening rom")
+	fROM, err = os.OpenFile("/tmp/snes/mem/rom", os.O_RDWR, 0666)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "open(rom): %v\n", err)
+		return
+	}
+	defer fROM.Close()
+	fmt.Printf("fd: %d\n", fROM.Fd())
+	romFile = fROM
 
 	//nmiFile, err = os.OpenFile("/tmp/snes/sig/nmi", os.O_RDONLY, 0666)
 	//if err != nil {
@@ -61,10 +69,19 @@ func main() {
 
 	ch := make(chan struct{})
 
+	// read rom header:
+	var romTitle [21]byte
+	_, err = romFile.ReadAt(romTitle[:], 0x7FC0)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "read(rom): %v\n", err)
+		return
+	}
+	fmt.Printf("rom title: `%s`\n", strings.TrimRight(string(romTitle[:]), " \000"))
+
 	go func() {
 		fmt.Println("hello from goroutine")
 
-		var wram [131072]byte
+		var wram [0x20000]byte
 		var nmiSignal [1]byte
 		for {
 			var n int
@@ -77,14 +94,32 @@ func main() {
 			//fmt.Println("NMI")
 			_ = nmiSignal
 
-			// read all bytes from WRAM:
-			n, err = wramFile.ReadAt(wram[:], 0)
+			// read one byte from WRAM:
+			n, err = wramFile.ReadAt(wram[0x10:0x11], 0x10)
 			if n == 0 {
 				continue
 			}
 			fmt.Printf("wram[$10] = %02x\n", wram[0x10])
 
 			if wram[0x10] == 0x14 {
+				break
+			}
+		}
+
+		var vram [0x10000]byte
+		for {
+			_, err = vramFile.ReadAt(vram[0xE000:], 0xE000)
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "readat(vram): %v\n", err)
+			}
+
+			fmt.Println(hex.Dump(vram[0xE000:]))
+
+			_, err = wramFile.ReadAt(wram[0x10:0x11], 0x10)
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "readat(wram): %v\n", err)
+			}
+			if wram[0x10] == 0x07 {
 				break
 			}
 		}
